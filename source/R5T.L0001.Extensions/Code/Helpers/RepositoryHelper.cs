8e449f0b1c3526969a3c4ba8bafb9c1b5db3941c
David Coats <david@getsafetycone.com>
2020-08-10 10:29:10 -0700

    Added RepositoryHelper methods to discover repository paths from repository contained paths.

--- source/R5T.L0001.Extensions/Code/Helpers/RepositoryHelper.cs
+++ source/R5T.L0001.Extensions/Code/Helpers/RepositoryHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using LibGit2Sharp;
+
 
 namespace R5T.L0001
 {
@@ -15,6 +17,25 @@ namespace R5T.L0001
         {
             var wasFound = repositoryDiscoveryResult != RepositoryHelper.NotFoundRepositoryDiscoveryResult;
             return wasFound;
+        }
+
+        public static bool TryDiscoverRepositoryPath(string path, out string repositoryPath)
+        {
+            repositoryPath = Repository.Discover(path);
+
+            var wasFound = RepositoryHelper.WasFound(repositoryPath);
+            return wasFound;
+        }
+
+        public static string DiscoverRepositoryPath(string path)
+        {
+            var wasFound = RepositoryHelper.TryDiscoverRepositoryPath(path, out var repositoryPath);
+            if (!wasFound)
+            {
+                throw new InvalidOperationException($"No Git repository found for '{path}'.");
+            }
+
+            return repositoryPath;
         }
     }
 }
