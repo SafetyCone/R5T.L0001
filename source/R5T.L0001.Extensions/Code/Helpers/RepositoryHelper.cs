d0e76e96f80a9f695e5216074ac048a12f933946
David Coats <david@getsafetycone.com>
2020-08-09 22:57:07 -0700

    Added R5T.L0001.Extensions library.

---
+++ source/R5T.L0001.Extensions/Code/Helpers/RepositoryHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace R5T.L0001
+{
+    public static class RepositoryHelper
+    {
+        public const string NotFoundRepositoryDiscoveryResult = null;
+
+
+        /// <summary>
+        /// Evaluates the output of <see cref="LibGit2Sharp.Repository.Discover(string)"/> to determine if a repository was discovered.
+        /// </summary>
+        public static bool WasFound(string repositoryDiscoveryResult)
+        {
+            var wasFound = repositoryDiscoveryResult != RepositoryHelper.NotFoundRepositoryDiscoveryResult;
+            return wasFound;
+        }
+    }
+}
